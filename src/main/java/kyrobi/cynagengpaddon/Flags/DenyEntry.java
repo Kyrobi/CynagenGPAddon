e97aa38e33bba3618f2d8df64fe216646800bb50
Kyrobi <lightling123jr@gmail.com>
2025-12-29 19:34:06 -0700

    Teleport to spawn if deny entry

--- src/main/java/kyrobi/cynagengpaddon/Flags/DenyEntry.java
+++ src/main/java/kyrobi/cynagengpaddon/Flags/DenyEntry.java
@@ -1,9 +1,11 @@
 package kyrobi.cynagengpaddon.Flags;
 
+import com.earth2me.essentials.spawn.EssentialsSpawn;
 import kyrobi.cynagengpaddon.CynagenGPAddon;
 import kyrobi.cynagengpaddon.Storage.ClaimData;
 import me.ryanhamshire.GriefPrevention.Claim;
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -18,10 +20,12 @@ import static kyrobi.cynagengpaddon.Storage.Datastore.myDataStore;
 public class DenyEntry implements Listener {
 
     private CynagenGPAddon plugin;
+    private EssentialsSpawn essSpawn;
 
     public DenyEntry(CynagenGPAddon plugin){
         this.plugin = plugin;
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        this.essSpawn = (EssentialsSpawn) Bukkit.getServer().getPluginManager().getPlugin("EssentialsSpawn");
     }
 
     @EventHandler
@@ -41,6 +45,9 @@ public class DenyEntry implements Listener {
             if(claimData.getNoEnterPlayer().contains(event.getPlayer().getUniqueId().toString())){
                 event.setCancelled(true);
                 player.sendMessage("You cannot enter this claim.");
+
+                Location spawn = essSpawn.getSpawn("default");
+                player.teleportAsync(spawn);
             }
         }
     }
